d695fe7e4e4d55f7e709e3b22579fea810595ed1
Shivanshu Mishra <shivanshu.mishra97@gmail.com>
2022-08-14 09:18:41 +0530

    Algorithm to reverse Linked List

---
+++ src/main/java/com/shivanshu/in/madeeasy/linkedlist/ReverseLinkedList.java
@@ -0,0 +1,32 @@
+package com.shivanshu.in.madeeasy.linkedlist;
+
+import com.shivanshu.in.madeeasy.linkedlist.circleexist.Node;
+
+/**
+ * Algortihm to reverse Single linked list
+ */
+public class ReverseLinkedList {
+    public Node reverse(Node head) {
+        Node current = head;
+        Node next = current.getNextNode();
+        Node previous = null;
+        while (current != null) {
+            current.setNextNode(previous);
+            previous = current;
+            current = next;
+            if (current != null)
+                next = current.getNextNode();
+        }
+        return previous;
+    }
+
+    public void print(Node node) {
+        int pos = 1;
+        while (node != null) {
+            System.out.printf("Node%s:%s\n", pos, node.getData().toString());
+            node = node.getNextNode();
+            pos++;
+        }
+
+    }
+}
---
+++ src/test/java/com/shivanshu/in/madeeasy/linkedlist/ReverseLinkedListTest.java
@@ -0,0 +1,30 @@
+package com.shivanshu.in.madeeasy.linkedlist;
+
+import com.shivanshu.in.madeeasy.linkedlist.circleexist.Node;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ReverseLinkedListTest {
+
+    @Test
+    public void testReverseLinkedList(){
+        ReverseLinkedList rll=new ReverseLinkedList();
+        Node<Integer> node1=new Node<>(1,null);
+        Node<Integer> node2=new Node<>(2,node1);
+        Node<Integer> node3=new Node<>(3,node2);
+        Node<Integer> head=new Node<>(4,node3);
+        System.out.println("Before");
+        rll.print(head);
+        Node<Integer> reverseHead=rll.reverse(head);
+        System.out.println("After");
+        rll.print(reverseHead);
+        Assert.assertEquals(node1,reverseHead);
+        Node<Integer> reverseNode2=reverseHead.getNextNode();
+        Assert.assertEquals(node2,reverseNode2);
+        Node<Integer> reverseNode3=reverseNode2.getNextNode();
+        Assert.assertEquals(node3,reverseNode3);
+        Node<Integer> reverseNode4=reverseNode3.getNextNode();
+        Assert.assertEquals(head,reverseNode4);
+        //rll.print(reverseNode1);
+    }
+}
